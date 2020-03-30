d957f80bcfb067b45e01742aa104d004bf49abe0
kevin-kili <kilikevin435@gmail.com>
2020-03-30 18:02:46 +0300

    added strikethough folder

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -1,15 +1,24 @@
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-like',
+  selector: 'like-box',
   templateUrl: './like.component.html',
   styleUrls: ['./like.component.css']
 })
 export class LikeComponent implements OnInit {
+  numberOfLikes : number = 0;
+  likeButtonClick() {
+    this.numberOfLikes++;
+  }
+  numberOfDislikes : number = 0;
+  dislikeButtonClick() {
+    this.numberOfDislikes++;
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef  } from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
+  
+  constructor(private elem:ElementRef) { }
 
-  constructor() { }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
 
+  }
 }
