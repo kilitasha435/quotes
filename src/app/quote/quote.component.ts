6fb6bf0cdf51cde3a63a1cbc571120e8d8500d06
kevin-kili <kilikevin435@gmail.com>
2020-04-02 00:30:43 +0300

    changed background image

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,7 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(1,'Author Holly', "Love yourself unconditionally first,then everything else is manageable.", 'Kevin Kili',new Date(2019,12,25)),
     new Quote(2,'Anonymous',"You are not lost, you are just early in the process", 'Eleanor Bwire',new Date(2019,5,17)),
-    new Quote(3,'Barack Obama','Never let an old flame burn you', 'Stacy',new Date(2020,1,23)),
+    new Quote(3,'Barack Obama','Never let an old flame burn you', 'Sheila',new Date(2020,1,23)),
     new Quote(4,'Tasha','It is not over unit it is all over','Eleanor',new Date(2019,8,21)),
 
   ];
